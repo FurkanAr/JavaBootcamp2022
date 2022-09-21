3ddc9729126469a6be03d4010e854d38f5bf92de
Furkan Ar <far.farnt@gmail.com>
2022-09-21 14:41:10 +0300

    first week

---
+++ JavaCamp2022/src/Week1/DataTypes/Main.java
@@ -0,0 +1,24 @@
+package Week1.DataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+			
+		int sayi = 12;
+		byte sayi2 = 102;
+		double sayi3 = 12.5;
+		
+		String sehir = "ANKARA";
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+	}
+
+}
